fbc6b9ef819cfad02db7b4d8722d5e0d924c8ad2
Pachito Marco Calabrese <pm.calabrese@gmail.com>
2017-01-15 02:33:03 +0100

    remove build

--- BUILD/NRF52_DK/GCC_ARM/mbed-os/features/netsocket/NetworkInterface.d
+++
@@ -1,9 +0,0 @@
-BUILD/NRF52_DK/GCC_ARM/mbed-os/features/netsocket/NetworkInterface.o: \
- mbed-os/features/netsocket/NetworkInterface.cpp \
- BUILD/NRF52_DK/GCC_ARM/mbed_config.h \
- mbed-os/features/netsocket/NetworkInterface.h \
- mbed-os/features/netsocket/nsapi_types.h \
- mbed-os/features/netsocket/SocketAddress.h \
- mbed-os/features/netsocket/nsapi_types.h mbed-os/platform/toolchain.h \
- mbed-os/platform/mbed_preprocessor.h \
- mbed-os/features/netsocket/NetworkStack.h
